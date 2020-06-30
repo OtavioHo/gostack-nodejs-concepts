1940f8e810383400876f24ef0c35a750da2ef133
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2020-06-30 15:57:52 -0300

    Creating all endpoints

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { uuid } = require("uuidv4"); 
 const cors = require("cors");
 
 // const { uuid } = require("uuidv4");
@@ -11,23 +12,51 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex(el => el.id == id);
+  if (index < 0) return response.status(404).json({error: "Repository not found."});
+
+  if (title) repositories[index].title = title;
+  if (url) repositories[index].url = url;
+  if (techs) repositories[index].techs = techs;
+
+  return response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(el => el.id == id);
+
+  if (index < 0) return response.status(404).json({error: "Repository not found."});
+
+  repositories.splice(index, 1);
+
+  return response.status(204)
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(el => el.id == id);
+
+  if (index < 0) return response.status(404).json({error: "Repository not found."});
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
